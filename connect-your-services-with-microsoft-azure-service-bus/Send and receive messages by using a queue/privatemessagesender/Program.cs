2fa1515d7420c8f09f79e27e4a5a6a9da5930f41
Theron Bueno <42697653+proDev-Theron@users.noreply.github.com>
2023-04-13 11:36:43 +0800

    Finish program.cs

--- connect-your-services-with-microsoft-azure-service-bus/Send and receive messages by using a queue/privatemessagesender/Program.cs
+++ connect-your-services-with-microsoft-azure-service-bus/Send and receive messages by using a queue/privatemessagesender/Program.cs
@@ -23,19 +23,37 @@ namespace privatemessagesender
         static async Task SendSalesMessageAsync()
         {
             // Create a Service Bus client here
+            // By leveraging "await using", the DisposeAsync method will be called automatically once the client variable goes out of scope. 
+            // In more realistic scenarios, you would want to store off a class reference to the client (rather than a local variable) so that it can be used throughout your program.
+
+            await using var client = new ServiceBusClient(ServiceBusConnectionString);
 
             // Create a sender here
+            await using ServiceBusSender sender = client.CreateSender(QueueName);
 
             try
             {
                 // Create and send a message here
+                string messageBody = $"$10,000 order for bicycle parts from retailer Adventure Works.";
+                var message = new ServiceBusMessage(messageBody);
+                Console.WriteLine($"Sending message: {messageBody}");
+                await sender.SendMessageAsync(message);
             }
+
             catch (Exception exception)
             {
                 Console.WriteLine($"{DateTime.Now} :: Exception: {exception.Message}");
             }
 
             // Close the connection to the sender here
+            
+            finally
+            {
+                // Calling DisposeAsync on client types is required to ensure that network
+                // resources and other unmanaged objects are properly cleaned up.
+                await sender.DisposeAsync();
+                await client.DisposeAsync();
+            }
         }
     }
 }
